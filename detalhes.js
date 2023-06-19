aac4168646ef0c0fa11b842c132114d1df1886a1
FelippeMagarao <126483942+FelippeMagarao@users.noreply.github.com>
2023-06-19 20:18:21 -0300

    Update detalhes.js

--- detalhes.js
+++ detalhes.js
@@ -6,7 +6,7 @@ let divDetalhes = document.getElementById('detalhes');
 let btvoltar = document.getElementById('voltar');
 
 btvoltar.addEventListener('click', () => {
-  window.location.href = '../';
+  window.location.href = './index.html';
 });
 
 function lerInfos() {
